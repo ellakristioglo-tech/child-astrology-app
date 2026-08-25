e48cd937b6f6cd4aca7cf78d6320e129152b824b
ellakristioglo-tech <ellakristioglo@gmail.com>
2026-08-25 17:44:11 +0200

    Add branded install icons and mobile bottom navigation

---
+++ ui-translations-fix.js
@@ -0,0 +1,35 @@
+Object.assign(translations.nl,{
+  home_title:'Welkom bij Child Astrology',
+  nav_children_short:'Kinderen',
+  nav_consultation_short:'Consultaties',
+  note_title_placeholder:'Bijvoorbeeld: observatie van de stemming',
+  note_content_placeholder:'Beschrijf je observaties...',
+  note_tags_placeholder:'#gedrag #emoties #ontwikkeling'
+});
+
+Object.assign(translations.ru,{
+  home_title:'Добро пожаловать в Child Astrology',
+  nav_children_short:'Дети',
+  nav_consultation_short:'Консультации',
+  note_title_placeholder:'Например: наблюдение за настроением',
+  note_content_placeholder:'Опишите ваши наблюдения...',
+  note_tags_placeholder:'#поведение #эмоции #развитие'
+});
+
+Object.assign(translations.ua,{
+  home_title:'Ласкаво просимо до Child Astrology',
+  nav_children_short:'Діти',
+  nav_consultation_short:'Консультації',
+  note_title_placeholder:'Наприклад: спостереження за настроєм',
+  note_content_placeholder:'Опишіть ваші спостереження...',
+  note_tags_placeholder:'#поведінка #емоції #розвиток'
+});
+
+Object.assign(translations.en,{
+  home_title:'Welcome to Child Astrology',
+  nav_children_short:'Children',
+  nav_consultation_short:'Consultations',
+  note_title_placeholder:'For example: mood observation',
+  note_content_placeholder:'Describe your observations...',
+  note_tags_placeholder:'#behaviour #emotions #development'
+});

53445d887a313ebbf82a2db9d689b592bac4baad
ellakristioglo-tech <ellakristioglo@gmail.com>
2026-08-26 15:01:16 +0200

    Add Family Scent Code: update ui-translations-fix.js

--- ui-translations-fix.js
+++ ui-translations-fix.js
@@ -5,6 +5,7 @@ Object.assign(translations.nl,{
   note_title_placeholder:'Bijvoorbeeld: observatie van de stemming',
   note_content_placeholder:'Beschrijf je observaties...',
   note_tags_placeholder:'#gedrag #emoties #ontwikkeling'
+  ,nav_scent:'Family Scent',action_scent:'Family Scent Code',action_scent_desc:'Eén gezamenlijke geur, berekend uit jullie geboortehoroscopen'
 });
 
 Object.assign(translations.ru,{
@@ -14,6 +15,7 @@ Object.assign(translations.ru,{
   note_title_placeholder:'Например: наблюдение за настроением',
   note_content_placeholder:'Опишите ваши наблюдения...',
   note_tags_placeholder:'#поведение #эмоции #развитие'
+  ,nav_scent:'Аромат семьи',action_scent:'Family Scent Code',action_scent_desc:'Один общий аромат, рассчитанный по натальным картам семьи'
 });
 
 Object.assign(translations.ua,{
@@ -23,6 +25,7 @@ Object.assign(translations.ua,{
   note_title_placeholder:'Наприклад: спостереження за настроєм',
   note_content_placeholder:'Опишіть ваші спостереження...',
   note_tags_placeholder:'#поведінка #емоції #розвиток'
+  ,nav_scent:'Аромат сім’ї',action_scent:'Family Scent Code',action_scent_desc:'Один спільний аромат, розрахований за натальними картами сім’ї'
 });
 
 Object.assign(translations.en,{
@@ -32,4 +35,5 @@ Object.assign(translations.en,{
   note_title_placeholder:'For example: mood observation',
   note_content_placeholder:'Describe your observations...',
   note_tags_placeholder:'#behaviour #emotions #development'
+  ,nav_scent:'Family Scent',action_scent:'Family Scent Code',action_scent_desc:'One shared scent calculated from your family birth charts'
 });

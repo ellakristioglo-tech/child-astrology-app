884affdfcf98a934a9988ffd3be0bcea63b3935a
ellakristioglo-tech <ellakristioglo@gmail.com>
2026-08-26 23:08:29 +0200

    Translate mobile methods navigation

--- ui-translations-fix.js
+++ ui-translations-fix.js
@@ -2,6 +2,8 @@ Object.assign(translations.nl,{
   home_title:'Welkom bij Child Astrology',
   nav_children_short:'Kinderen',
   nav_consultation_short:'Consultaties',
+  nav_scent_short:'Geur',
+  nav_methods_menu:'Methodes',
   note_title_placeholder:'Bijvoorbeeld: observatie van de stemming',
   note_content_placeholder:'Beschrijf je observaties...',
   note_tags_placeholder:'#gedrag #emoties #ontwikkeling'
@@ -12,6 +14,8 @@ Object.assign(translations.ru,{
   home_title:'Добро пожаловать в Child Astrology',
   nav_children_short:'Дети',
   nav_consultation_short:'Консультации',
+  nav_scent_short:'Аромат',
+  nav_methods_menu:'Методы',
   note_title_placeholder:'Например: наблюдение за настроением',
   note_content_placeholder:'Опишите ваши наблюдения...',
   note_tags_placeholder:'#поведение #эмоции #развитие'
@@ -22,6 +26,8 @@ Object.assign(translations.ua,{
   home_title:'Ласкаво просимо до Child Astrology',
   nav_children_short:'Діти',
   nav_consultation_short:'Консультації',
+  nav_scent_short:'Аромат',
+  nav_methods_menu:'Методи',
   note_title_placeholder:'Наприклад: спостереження за настроєм',
   note_content_placeholder:'Опишіть ваші спостереження...',
   note_tags_placeholder:'#поведінка #емоції #розвиток'
@@ -32,6 +38,8 @@ Object.assign(translations.en,{
   home_title:'Welcome to Child Astrology',
   nav_children_short:'Children',
   nav_consultation_short:'Consultations',
+  nav_scent_short:'Scent',
+  nav_methods_menu:'Methods',
   note_title_placeholder:'For example: mood observation',
   note_content_placeholder:'Describe your observations...',
   note_tags_placeholder:'#behaviour #emotions #development'

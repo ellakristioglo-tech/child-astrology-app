3b0671180372b3e45f9a664d984b197d56270338
ellakristioglo-tech <ellakristioglo@gmail.com>
2026-08-26 21:18:27 +0200

    Add privacy-safe app analytics (analytics-config.js)

---
+++ analytics-config.js
@@ -0,0 +1,5 @@
+window.CHILD_ASTROLOGY_ANALYTICS = Object.freeze({
+  // Insert the GA4 web stream ID here, for example: G-ABC123DE45.
+  measurementId: '',
+  dashboardUrl: 'https://analytics.google.com/analytics/web/'
+});

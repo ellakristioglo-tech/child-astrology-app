633681b854f002b7228fa024da25cb98f25cdc5b
ellakristioglo-tech <ellakristioglo@gmail.com>
2026-08-27 08:42:26 +0200

    Use production Google Analytics stream

--- analytics-config.js
+++ analytics-config.js
@@ -1,4 +1,4 @@
 window.CHILD_ASTROLOGY_ANALYTICS = Object.freeze({
-  measurementId: 'G-JB96Z7T13G',
+  measurementId: 'G-SZHFB9KVM4',
   dashboardUrl: 'https://analytics.google.com/analytics/web/'
 });
